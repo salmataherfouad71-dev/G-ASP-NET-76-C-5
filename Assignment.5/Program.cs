1539fe69bf8cfdd6c1c16ad1dc2dd97bcb1cc45c
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-07-31 19:38:02 +0300

    Question01

--- Assignment.5/Program.cs
+++ Assignment.5/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            #region Question01
+            ///*Add a private string password ="secret";field to a Book class.
+            // try to print it from Main(outside the class).What happend and why?*/
+            //Book book = new Book();
+            ////Console.WriteLine(book.password);
+            ///*It is a compile-time error because the private field is only accessable in 
+            // the same class.*/
+            #endregion
         }
     }
 }
